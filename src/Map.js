8d0f8be9a82eeddaf8d5cc7698a76381081d7f6c
Daniel Segovia <daniel@dabici.mx>
2017-06-13 12:47:28 -0500

    added the -1 case to setClassName, changed that = this to a more simple approach (saving memory), and added the player XY coords to display in the map

--- src/Map.js
+++ src/Map.js
@@ -4,6 +4,10 @@ class Map extends React.Component {
 	setClassName(td){
 		var className = 'square'
 		switch(td){
+			case -1:
+				className += ' player'
+				break;
+				
 			case 0:
 				className += ' wall'
 				break;
@@ -19,14 +23,19 @@ class Map extends React.Component {
 	}
 	render(){
 		var map = this.props.map
-		var that = this
+		var setClassName = this.setClassName
+		var playerXcoord = this.props.playerCoords[0]
+		var playerYcoord = this.props.playerCoords[1]
 		return(
 			<table>
 				<tbody>
 					{map.map(function(tr, index){
 						return <tr key={index}>
-							{map[index].map(function(td, index2){
-								return <td key={index2} className={that.setClassName(td)}></td>
+							{map[index].map(function(td, index2){	
+								if(playerXcoord === index && playerYcoord === index2){
+									td = -1
+								}
+								return <td key={index2} className={setClassName(td)}></td>
 							})}
 						</tr>
 					})}
