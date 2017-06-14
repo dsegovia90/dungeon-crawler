78b587e03b6cba9ae4583ee02441f2d459a900ee
Daniel Segovia <daniel@dabici.mx>
2017-06-13 20:03:33 -0500

    grid size to 25

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App.js'
 import './index.css'
 
-ReactDOM.render(<App gridSize={30} />, document.getElementById('root'));
+ReactDOM.render(<App gridSize={25} />, document.getElementById('root'));
