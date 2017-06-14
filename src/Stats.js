796cb8cc44732fb84f4e7610bf4e6de23e01304a
Daniel Segovia <daniel@dabici.mx>
2017-06-13 19:37:58 -0500

    created file

---
+++ src/Stats.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Stats(props){
+  return(
+    <div>
+      <div className='stats-div'>
+        <h3 className='stats'>HP: {props.hp}</h3>
+        <h3 className='stats'>Attack: {props.attack}</h3>
+        <h3 className='stats'>Lvl: {props.level}</h3>
+        <h3 className='stats'>Floor: {props.floor}</h3>
+      </div>
+      <br />
+    </div>
+  )
+}
+
+export default Stats
