f5dee60781344e0d51df452ead9b7d06075973b8
Daniel Segovia <daniel@dabici.mx>
2017-06-16 16:55:28 -0500

    added maxHp to HP: h3 tag

--- src/Stats.js
+++ src/Stats.js
@@ -4,7 +4,7 @@ function Stats(props){
   return(
     <div>
       <div className='stats-div'>
-        <h3 className='stats'>HP: {props.hp}</h3>
+        <h3 className='stats'>HP: {props.hp}/{props.maxHp}</h3>
         <h3 className='stats'>Attack: {props.attack}</h3>
         <h3 className='stats'>Lvl: {props.level}</h3>
         <h3 className='stats'>Floor: {props.floor}</h3>
